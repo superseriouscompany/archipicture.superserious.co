8dc5e8335feace48d630f49ab1394c54d4b61d57
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-02-26 01:53:30 +0000

    Add CORS support

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
+const cors       = require('cors');
 const app        = express();
 
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
+app.use(cors())
 app.disable('etag');
 app.get('/', function(req, res) {
   res.json({cool: 'nice'})
