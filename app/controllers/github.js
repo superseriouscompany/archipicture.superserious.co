09eada3a417a23e02b51eace4cac0fd0f89d9b47
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-02-26 02:11:31 +0000

    Redirect back to the frontend after creating user

--- app/controllers/github.js
+++ app/controllers/github.js
@@ -1,5 +1,8 @@
 const request = require('request-promise');
+const shortid = require('shortid');
 const config  = require('../config');
+
+const users = {}
 
 module.exports = function(app) {
   app.get('/callback', callback);
@@ -15,8 +18,9 @@ function callback(req, res, next) {
       code:          req.query.code,
     }
   }).then((json) => {
-    res.json({
-      token: json.access_token,
-    })
+    if( !json.access_token ) { throw new Error(`Invalid json ${JSON.stringify(json)}`) }
+    const id = shortid.generate()
+    users[json.access_token] = { id: id }
+    res.redirect(`http://localhost:8000?id=${id}`)
   }).catch(next);
 }
